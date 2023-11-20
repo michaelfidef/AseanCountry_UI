f6d20975fb710fffa458478c215babf4daf273bc
michaelfidef <michaelfidef22@gmail.com>
2023-11-20 12:10:36 +0700

    Add profile activity

--- app/src/main/java/negara/asean/aseancountry/ProfileActivity.kt
+++ app/src/main/java/negara/asean/aseancountry/ProfileActivity.kt
@@ -1,21 +1,68 @@
 package negara.asean.aseancountry
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProfileActivity(
     modifier: Modifier = Modifier
 ) {
-    Box(
+    Surface(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(stringResource(R.string.menu_profile))
+        color = Color.White) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .background(Color.LightGray)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            // TextView for Name
+            Text(
+                text = stringResource(R.string.nama_user),
+                modifier = Modifier.padding(5.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 23.sp
+            )
+
+            // TextView for Email
+            Text(
+                text = stringResource(R.string.email_user),
+                modifier = Modifier.padding(5.dp),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }

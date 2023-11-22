122f13018ce0f3fe221d6ed6a2c75f3d136da445
michaelfidef <michaelfidef22@gmail.com>
2023-11-22 13:21:56 +0700

    Final Submission Dikoding Jetpack Compose

--- app/src/main/java/negara/asean/aseancountry/model/CountriesData.kt
+++ app/src/main/java/negara/asean/aseancountry/model/CountriesData.kt
@@ -10,8 +10,13 @@ object CountriesData {
             R.drawable.kamboja,
             "Phnom Penh",
             "Kamboja terkenal dengan kuil-kuil Angkor Wat yang megah " +
-                    "dan memiliki potensi pariwisata yang besar." +
-                    "Mata uang yang digunakan adalah Riel Kamboja (KHR) "
+                    "dan memiliki potensi pariwisata yang besar. " +
+                    "Mata uang yang digunakan adalah Riel Kamboja (KHR). " +
+                    "Terakhir adalah Kamboja, negara di Asia Tenggara ini resmi gabung ASEAN " +
+                    "pada 30 April 1999. Saat itu, negara yang dipimpin oleh seorang raja " +
+                    "ini dinyatakan resmi bergabung dalam upacara khusus penerimaan di Hanoi, Vietnam. " +
+                    "Selama sekitar 14 tahun bergabung dengan ASEAN, Kamboja sudah beberapa kali " +
+                    "menjadi tuan rumah KTT ASEAN. Terbaru pada 10-13 November 2022 di Phnom Penh. "
         ),
         Country(
             2,
@@ -20,7 +25,11 @@ object CountriesData {
             "Bandar Seri Begawan",
             "Brunei Darussalam adalah negara kecil di pantai utara pulau Kalimantan yang " +
                     "kaya akan sumber daya alam, terutama minyak dan gas alam. " +
-                    "Mata uang yang digunakan adalah Dolar Brunei (BND) "
+                    "Mata uang yang digunakan adalah Dolar Brunei (BND). " +
+                    "Dengan ciri khas bendera berwarna kuning, hitam, putih, dengan lambang kenegaraan " +
+                    "di bagian tengahnya, Brunei Darussalam menjadi negara ke-6 yang gabung ASEAN. " +
+                    "Resmi bergabung pada 8 Januari 1984, Brunei Darussalam " +
+                    "merupakan negara dengan bentuk pemerintahan kesultanan (kerajaan Islam)."
         ),
         Country(
             3,
@@ -29,7 +38,11 @@ object CountriesData {
             "Jakarta",
             "Negara Republik Indonesia adalah negara kepulauan terbesar di dunia dengan " +
                     "populasi yang besar dan beragam budaya serta kekayaan alam. " +
-                    "Mata uang yang digunakan adalah Rupiah Indonesia (IDR) "
+                    "Mata uang yang digunakan adalah Rupiah Indonesia (IDR). " +
+                    "Merdeka pada 17 Agustus 1945, Indonesia termasuk salah satu pendiri ASEAN " +
+                    "yang telah bergabung sejak 8 Agustus 1967. Hebatnya lagi, " +
+                    "Indonesia menjadi tuan rumah ASEAN Summit pertama yang digelar pada " +
+                    "23-24 Februari 1976 di Bali."
         ),
         Country(
             4,
@@ -38,7 +51,12 @@ object CountriesData {
             "Vientiane",
             "Laos adalah negara pegunungan yang indah " +
                     "dengan budaya yang kaya dan beragam. " +
-                    "Mata uang yang digunakan adalah Kip Laos (LAK) "
+                    "Mata uang yang digunakan adalah Kip Laos (LAK). " +
+                    "Resmi menjadi anggota ASEAN pada 23 Juli 1997, " +
+                    "Laos merupakan salah satu negara di Asia Tenggara yang menganut " +
+                    "sistem republik sosialis satu partai sebagai bentuk pemerintahan. Dengan kata lain, " +
+                    "kepala negara Laos dipimpin oleh presiden, dan pemerintahannya adalah perdana menteri. " +
+                    "Karena tergolong baru, sampai saat ini Laos baru dua kali menjadi tuan rumah KTT ASEAN. "
         ),
         Country(
             5,
@@ -47,7 +65,12 @@ object CountriesData {
             "Kuala Lumpur",
             "Malaysia terletak di Semenanjung Malaysia dan bagian utara " +
                     "pulau Borneo, dikenal dengan keindahan alamnya dan ekonomi yang berkembang. " +
-                    "Mata uang yang digunakan adalah Ringgit Malaysia (MYR) "
+                    "Mata uang yang digunakan adalah Ringgit Malaysia (MYR). " +
+                    "Malaysia turut menjadi salah satu negara pendiri ASEAN. Sebagai negara di Asia Tenggara " +
+                    "yang resmi merdeka pada 31 Agustus 1957, Malaysia dipimpin oleh seorang raja " +
+                    "dengan gelar “Yang Dipertuan Agong”. Kala itu, Yang Pertuan Agong " +
+                    "pertama dipimpin oleh Yang Dipertuan Besar Negeri Sembilan, Tuanku Abdul " +
+                    "Rahman Ibni Almarhum Tuanku Muhammad (1957-1960). "
         ),
         Country(
             6,
@@ -57,7 +80,10 @@ object CountriesData {
             "Myanmar adalah negara dengan potensi ekonomi yang berkembang pesat, " +
                     "tetapi juga menghadapi berbagai tantangan politik dan sosial. " +
                     "Negara ini juga memiliki banyak situs bersejarah dan kuil Budha yang indah. " +
-                    "Mata uang yang digunakan adalah Kyat Myanmar (MMK)"
+                    "Mata uang yang digunakan adalah Kyat Myanmar (MMK). " +
+                    "Sama halnya dengan Laos, Myanmar resmi bergabung di ASEAN pada 23 Juli 1997. " +
+                    "Sebagai negara yang menganut sistem republik, tentu saja Myanmar dipimpin oleh seorang presiden. " +
+                    "Negara Myanmar sendiri telah merdeka sekitar 48 tahun lalu, atau tepatnya pada 4 Januari 1948. "
         ),
         Country(
             7,
@@ -66,7 +92,10 @@ object CountriesData {
             "Manila",
             "Filipina adalah negara kepulauan di Asia Tenggara dengan populasi " +
                     "yang besar dan beragam, serta keindahan alam yang memukau. " +
-                    "Mata uang yang digunakan adalah Peso Filipina (PHP)"
+                    "Mata uang yang digunakan adalah Peso Filipina (PHP). " +
+                    "Merdeka pada 4 Juli 1946, Filipina turut menjadi salah satu negara pendiri ASEAN. " +
+                    "Negara kepulauan yang identik dengan bendera berwarna biru, merah, dan putih ini memiliki bentuk  " +
+                    "pemerintahan Republik. Presiden dipilih langsung melalui pemilu untuk memimpin negara selama 6 tahun. "
         ),
         Country(
             8,
@@ -75,7 +104,12 @@ object CountriesData {
             "Singapura",
             "Singapura adalah negara pulau yang sangat maju secara ekonomi " +
                     "dan dikenal sebagai pusat keuangan dan perdagangan internasional. " +
-                    "Mata uang yang digunakan adalah Dolar Singapura (SGD)"
+                    "Mata uang yang digunakan adalah Dolar Singapura (SGD). " +
+                    "Masih menjadi salah satu pendiri ASEAN, Singapura merupakan negara di Asia Tenggara " +
+                    "yang memiliki bentuk pemerintahan republik parlementer. Itu artinya, kepala negara  " +
+                    "di Singapura dijabat oleh presiden, namun pemerintahan dipimpin oleh perdana menteri. " +
+                    "Telah merdeka selama 48 tahun, Singapura menjadi tuan rumah langganan KTT ASEAN, " +
+                    "yakni pada 1992, 2007, dan 2018. "
         ),
         Country(
             9,
@@ -84,7 +118,12 @@ object CountriesData {
             "Bangkok",
             "Thailand adalah negara dengan budaya yang kaya dan terkenal dengan pantai-pantai " +
                     "indah, kuil-kuil yang megah, dan makanan lezat seperti tomyum dan " +
-                    "pad thai yang terkenal di seluruh dunia." + "Mata uang yang digunakan adalah Baht Thailand (THB)"
+                    "pad thai yang terkenal di seluruh dunia." + "Mata uang yang digunakan adalah Baht Thailand (THB). " +
+                    "Tak hanya menjadi negara pelopor, Thailand turut menjadi tempat lahirnya ASEAN. " +
+                    " Dibandingkan negara lainnya, Thailand menjadi negara yang paling banyak menjadi tuan rumah KTT ASEAN, " +
+                    "yakni pada KTT ASEAN ke-5, 14, 15, 16, 34, dan 35. " +
+                    "Memiliki bentuk pemerintahan monarki konstitusional atau Kerajaan, Thailand dipimpin oleh seorang raja. " +
+                    "Hanya saja, pemerintahan di Thailand dijalankan oleh kabinet perdana menteri. "
         ),
         Country(
             10,
@@ -94,7 +133,10 @@ object CountriesData {
             "Timor Leste adalah negara yang relatif baru dengan sejarah yang kompleks " +
                     "dan kekayaan alam yang potensial. " +
                     "Mata uang yang digunakan adalah Dolar Amerika Serikat (USD) " +
-                    "digunakan bersama dengan mata uang lokal, Kina Timor (TMT)"
+                    "digunakan bersama dengan mata uang lokal, Kina Timor (TMT). " +
+                    "sejak November 2022, secara prinsip, ASEAN telah menerima Timor Leste sebagai anggota. " +
+                    "Oleh karena itu, meski belum punya hak suara, Timor Leste telah mengikuti " +
+                    "aneka pertemuan ASEAN selama Indonesia menjadi ketua bergilir pada 2023."
         ),
         Country(
             11,
@@ -103,7 +145,11 @@ object CountriesData {
             "Hanoi",
             "Vietnam adalah negara yang memiliki sejarah panjang dan terkenal " +
                     "dengan lanskap alam yang indah serta warisan budayanya." +
-                    "Mata uang yang digunakan adalah Dong Vietnam (VND)"
+                    "Mata uang yang digunakan adalah Dong Vietnam (VND). " +
+                    "Selanjutnya Vietnam, anggota ke-7 ASEAN yang resmi bergabung pada 28 Juli 1995. " +
+                    "Negara dengan ibu kota Hanoi ini merdeka sejak 2 September 1945, dan memiliki bentuk pemerintahan sistem " +
+                    "republik komunis. Artinya, negara ini hanya punya satu partai, dan memiliki presiden " +
+                    "sebagai kepala negara. "
         )
     )
 }
